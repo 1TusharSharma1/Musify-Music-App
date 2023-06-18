c5452b4a461d67d70a554bdbd5fc91331b8d8506
Tushar Sharma <tush.sharma2504@gmail.com>
2023-06-19 00:10:00 +0530

    Added Songs & Improved functionalities

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log("Musify for the win!");
 let songIndex = 0;
-let audioElement = new Audio('./Songs/Starboy.mp3');
+let audioElement = new Audio('./Songs/2.mp3');
     // audioElement.play();
 let masterPlay = document.getElementById('masterPlay');
 let gif  = document.getElementById('songBanner');
@@ -8,21 +8,16 @@ let myProgressBar = document.getElementById('myProgressBar');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let Songs = [
-    {songName: "Let Me Love You",filePath: "./Songs/LMLY.mp3" , coverPath: "https://c.saavncdn.com/273/Encore-English-2016-20190419221937-500x500.jpg"},
-    {songName: "Call Out My Name",filePath: "./Songs/Starboy.mp3" , coverPath: "https://upload.wikimedia.org/wikipedia/en/3/39/The_Weeknd_-_Starboy.png"},
-    {songName: "Attention",filePath: "./Songs/LMLY.mp3" , coverPath: "https://wallpapercave.com/dwp1x/wp10168148.jpg"},
-    {songName: "Drag Me Down",filePath: "./Songs/LMLY.mp3" , coverPath: "https://upload.wikimedia.org/wikipedia/en/3/34/One_Direction_-_Drag_Me_Down_%28Official_Single_Cover%29.png"},
-    {songName: "ABCD",filePath: "./Songs/LMLY.mp3" , coverPath: " "},
-    {songName: "Let Me Love You",filePath: "./Songs/LMLY.mp3" , coverPath: " "},
-    {songName: "Let Me Love You",filePath: "./Songs/LMLY.mp3" , coverPath: " "},
-    {songName: "Let Me Love You",filePath: "./Songs/LMLY.mp3" , coverPath: " "},
-    {songName: "Let Me Love You",filePath: "./Songs/LMLY.mp3" , coverPath: " "},
-    {songName: "Let Me Love You",filePath: "./Songs/LMLY.mp3" , coverPath: " "}
+    {songName: "Let Me Love You",filePath: "./Songs/1.mp3" , coverPath: "https://c.saavncdn.com/273/Encore-English-2016-20190419221937-500x500.jpg"},
+    {songName: "Call Out My Name",filePath: "./Songs/2.mp3" , coverPath: "https://c.saavncdn.com/396/The-Highlights-English-2021-20210205104909-500x500.jpg"},
+    {songName: "Attention",filePath: "./Songs/3.mp3" , coverPath: "https://wallpapercave.com/dwp1x/wp10168148.jpg"},
+    {songName: "Drag Me Down",filePath: "./Songs/4.mp3" , coverPath: "https://upload.wikimedia.org/wikipedia/en/3/34/One_Direction_-_Drag_Me_Down_%28Official_Single_Cover%29.png"},
+    {songName: "Starboy",filePath: "./Songs/5.mp3" , coverPath: "https://upload.wikimedia.org/wikipedia/en/3/39/The_Weeknd_-_Starboy.png"},
+    {songName: "Calm Down",filePath: "./Songs/6.mp3" , coverPath: "https://i1.sndcdn.com/artworks-TbobgbPvSPkSitB0-biumjQ-t500x500.jpg"}
 ]
 
  songItems.forEach((element,i) => {
-    console.log(element,i);
-    
+    console.log(element,i);    
     element.getElementsByTagName("img")[0].src = Songs[i].coverPath;
     element.getElementsByTagName("span")[0].songName = Songs[i].songName;
  })
@@ -35,7 +30,6 @@ masterPlay.addEventListener('click', ()=>{
     else{
         audioElement.pause();
         gif.style.opacity = 0;
-
     }
 })
 
@@ -46,4 +40,24 @@ audioElement.addEventListener('timeupdate', ()=> {
 
 myProgressBar.addEventListener('change', ()=> {
     audioElement.currentTime = (myProgressBar.value/100)*audioElement.duration;
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+            element.classList.remove('fa-pause-circle');
+            element.classList.add('fa-play-circle');
+    })
+}
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{
+        makeAllPlays();
+        index  = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');        
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = Songs[index-1].filePath;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+
+    })
 })
